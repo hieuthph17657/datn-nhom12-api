a09e4d31d532331d09620d24dfc8adbd35351097
tranhuyhieu <81691983+tranhuyhieu@users.noreply.github.com>
2022-10-03 10:15:27 +0700

    Initial commit

---
+++ src/main/java/datnnhom12api/DatnNhom12ApiApplication.java
@@ -0,0 +1,13 @@
+package datnnhom12api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnNhom12ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnNhom12ApiApplication.class, args);
+	}
+
+}
