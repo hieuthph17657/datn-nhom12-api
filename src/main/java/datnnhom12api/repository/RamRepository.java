7215a2f7bd8864705ea6e6017a1b7eeed5abef8b
duc.nt <81727369+TrungDuc9998@users.noreply.github.com>
2022-11-26 00:10:41 +0700

    api cpu and ram

---
+++ src/main/java/datnnhom12api/repository/RamRepository.java
@@ -0,0 +1,10 @@
+package datnnhom12api.repository;
+
+import datnnhom12api.entity.RamEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RamRepository extends JpaRepository<RamEntity, Long>, JpaSpecificationExecutor<RamEntity> {
+}
