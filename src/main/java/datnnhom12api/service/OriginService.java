9ae8b929ff7e60f59916195f1bd73604ef289df7
TungPTPH17719 <tungptph17719@fpt.edu.vn>
2022-11-27 02:18:39 +0700

    tung origin

---
+++ src/main/java/datnnhom12api/service/OriginService.java
@@ -0,0 +1,21 @@
+package datnnhom12api.service;
+
+import datnnhom12api.core.Filter;
+import datnnhom12api.entity.CategoryEntity;
+import datnnhom12api.entity.OriginEntity;
+import datnnhom12api.exceptions.CustomException;
+import datnnhom12api.request.OriginRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OriginService {
+    OriginEntity save(OriginRequest post) throws CustomException;
+
+    OriginEntity edit(Long id, OriginRequest post) throws CustomException;
+
+    OriginEntity delete(Long id) throws CustomException;
+
+    Page<OriginEntity> paginate(int page, int limit, List<Filter> whereParams, Map<String, String> sortBy);
+}
