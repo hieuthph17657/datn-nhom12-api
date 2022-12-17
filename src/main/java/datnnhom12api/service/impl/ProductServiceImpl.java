95a003f4df4ea9ef421e64aac1bc2001b99dde66
tranhuyhieu <81691983+tranhuyhieu@users.noreply.github.com>
2022-12-17 11:45:03 +0700

    hieu_discount_product

--- src/main/java/datnnhom12api/service/impl/ProductServiceImpl.java
+++ src/main/java/datnnhom12api/service/impl/ProductServiceImpl.java
@@ -259,7 +259,7 @@ public class ProductServiceImpl implements ProductService {
                     }
                     productEntity = productEntityOptional.get();
                     productEntity.setDiscount(discountEntity);
-                    productEntity.setPrice(productEntity.getPrice() - (productEntity.getPrice() * discountEntity.getRatio() / 100));
+                    productEntity.setPrice(Math.ceil(productEntity.getPrice() - (productEntity.getPrice() * discountEntity.getRatio() / 100)));
                     productEntity = productRepository.save(productEntity);
                     listdiscountProduct.add(productEntity);
                 }
@@ -283,7 +283,7 @@ public class ProductServiceImpl implements ProductService {
             if (product.getDiscount()!=null&&product.getId()==idPro){
                 productEntity = productEntityOptional.get();
                 productEntity.setDiscount(null);
-                productEntity.setPrice(productEntity.getPrice()/((100- discountEntity.getRatio()) / 100));
+                productEntity.setPrice(Math.ceil(productEntity.getPrice()/((100- discountEntity.getRatio()) / 100)));
                 productEntity = productRepository.save(productEntity);
             }
         }
