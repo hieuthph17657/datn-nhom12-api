6a3b58ec24876ff514b63ec95605de348ee55ecb
duc.nt <81727369+TrungDuc9998@users.noreply.github.com>
2023-01-14 22:53:36 +0700

    fix code win request

--- src/main/java/datnnhom12api/entity/WinEntity.java
+++ src/main/java/datnnhom12api/entity/WinEntity.java
@@ -1,9 +1,7 @@
 package datnnhom12api.entity;
 
 import datnnhom12api.core.BaseEntity;
-import datnnhom12api.request.ScreenRequest;
 import datnnhom12api.request.WinRequest;
-import datnnhom12api.utils.support.ScreenStatus;
 import datnnhom12api.utils.support.WinStatus;
 import lombok.*;
 
@@ -32,9 +30,8 @@ public class WinEntity extends BaseEntity {
 
     public void setData(WinRequest request) {
         this.name = request.getName();
-        this.version= request.getVersion();
-        this.status = request.getStatus()
-                == WinStatus.DRAFT ? WinStatus.DRAFT
-                : WinStatus.ACTIVE ;
+        this.version = request.getVersion();
+        this.status = request.getStatus() == WinStatus.DRAFT ? WinStatus.DRAFT :
+                (request.getStatus() == WinStatus.ACTIVE ? WinStatus.ACTIVE : WinStatus.INACTIVE) ;
     }
 }
